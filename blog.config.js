64f7497f37b23fa829fd1c882a5a2e844e060b10
Wang Pt <33533584+wpttt@users.noreply.github.com>
2023-12-30 01:35:15 +0800

    add cusdis and its appid in blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -39,7 +39,7 @@ const BLOG = {
   },
   comment: {
     // support provider: gitalk, utterances, cusdis
-    provider: '', // leave it empty if you don't need any comment plugin
+    provider: 'cusdis', // leave it empty if you don't need any comment plugin
     gitalkConfig: {
       repo: '', // The repository of store comments
       owner: '',
@@ -52,7 +52,7 @@ const BLOG = {
       repo: ''
     },
     cusdisConfig: {
-      appId: '', // data-app-id
+      appId: 'aff60e5e-ed5b-4192-902d-25d7a74b3912', // data-app-id
       host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
