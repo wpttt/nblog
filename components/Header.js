553aa7052728a717b44ff7275dc3e704786c03e5
Wang Pt <33533584+wpttt@users.noreply.github.com>
2024-01-02 11:25:21 +0800

    Update Header.js add links

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,7 @@ const NavBar = () => {
   const links = [
     { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
     { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
-    { id: 2, name: locale.NAV.TIPS, to: '/tips', show: true },
+    { id: 2, name: locale.NAV.LINK, to: '/links', show: true },
     { id: 3, name: locale.NAV.RSS, to: '/feed', show: true, external: true },
     { id: 4, name: locale.NAV.SEARCH, to: '/search', show: true }
   ]
