1d58edbab0b0b95a017508b3dcf87d0960947b04
wpttt <wpttt@126.com>
2024-01-04 19:15:11 +0800

    add og image generator

--- blog.config.js
+++ blog.config.js
@@ -1,24 +1,24 @@
 const BLOG = {
-  title: 'WPTHERE',
+  title: 'PTHERE',
   author: 'wangpt',
   email: 'wpthere@hotmail.com',
   link: 'https://wpthere.top',
   description: '个人知识记录簿.',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
-  appearance: 'light', // ['light', 'dark', 'auto'],
+  appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2023, // If leave this empty, current year will be used.
-  postsPerPage: 7,
-  sortByDate: false,
+  postsPerPage: 10,
+  sortByDate: true,
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   //ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  ogImageGenerateURL: 'https://og-image-wpthere.vercel.app', // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: 'vercel-og-nextjs-cspumuchr-wpttts-projects.vercel.app', // 'https://og-image-wpthere.vercel.app'The link to generate OG image, don't end with a slash
   socialLink: 'https://weibo.com/u/2149581044',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
