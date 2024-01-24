6cca1612212020e61d7e2a97ef21cb51006ae683
Wang Pt <33533584+wpttt@users.noreply.github.com>
2024-01-24 11:28:18 +0800

    Update Header.js remove personal

--- components/Header.js
+++ components/Header.js
@@ -14,9 +14,9 @@ const NavBar = () => {
     //{ id: 2, name: locale.NAV.LINK, to: '/links', show: true },
     //{ id: 3, name: locale.NAV.READ, to: '/read', show: true },
     //{ id: 4, name: locale.NAV.LIFE, to: '/life', show: true },
-    { id: 2, name: locale.NAV.HOME, to: '/home', show: true },
-    { id: 3, name: locale.NAV.RSS, to: '/feed', show: true, external: true },
-    { id: 4, name: locale.NAV.SEARCH, to: '/search', show: true }
+    //{ id: 2, name: locale.NAV.HOME, to: '/home', show: true },
+    { id: 2, name: locale.NAV.RSS, to: '/feed', show: true, external: true },
+    { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true }
   ]
   return (
     <div className="flex-shrink-0">
